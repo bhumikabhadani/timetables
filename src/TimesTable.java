a7222142fdadeadd868c3d0c2e59246c461ec33c
bhumikabhadani <bhumikabhadani08@gmail.com>
2022-05-03 22:53:40 +0100

    second commit

--- src/TimesTable.java
+++ src/TimesTable.java
@@ -4,7 +4,7 @@ public class TimesTable  {
 
 
         int a = 2;
-        for(int i =1; i <= 12; i++){
+        for(int i =1; i <= 10; i++){
             System.out.println(a+"x" +i+ "=" + i*a);
 
         }
